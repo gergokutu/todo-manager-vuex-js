582c51b23740816d8bd0eb159443bc43e44db8c0
gergokutu <gergo.kutu@gmail.com>
2020-07-14 15:50:45 +0200

    Implement the delete todo icon

--- src/store/modules/todos.js
+++ src/store/modules/todos.js
@@ -39,7 +39,8 @@ const actions = {
 const mutations = {
   setTodos: (state, todos) => (state.todos = todos),
   newTodo: (state, todo) => state.todos.unshift(todo),
-  removeTodo: (state, id) => state.todos = state.todos.filter(todo => todo.id !== id)
+  removeTodo: (state, id) =>
+    (state.todos = state.todos.filter(todo => todo.id !== id))
 };
 
 export default {
