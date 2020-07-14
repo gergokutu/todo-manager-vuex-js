d443dd5dbb8b251a87ee498be05053e41fb3bb3a
gergokutu <gergo.kutu@gmail.com>
2020-07-14 12:28:57 +0200

    Send fetched data to store, fetch with axios, do not forget the commit

--- src/store/modules/todos.js
+++ src/store/modules/todos.js
@@ -1,25 +1,25 @@
-// import axios from 'axios';
+import axios from 'axios';
+// https://jsonplaceholder.typicode.com/todos
 
 const state = {
-  todos: [
-    {
-      id: 1,
-      title: 'Todo One'
-    },
-    {
-      id: 2,
-      title: 'Todo Two'
-    }
-  ]
+  todos: []
 };
 
 const getters = {
   allTodos: (state) => state.todos
 };
 
-const actions = {};
+const actions = {
+  async fetchTodos({ commit }) {
+    const response = await axios.get('https://jsonplaceholder.typicode.com/todos');
 
-const mutations = {};
+    commit('setTodos', response.data)
+  }
+};
+
+const mutations = {
+  setTodos: (state, todos) => (state.todos = todos)
+};
 
 export default {
   // ES6 > state: state...
