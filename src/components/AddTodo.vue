7a3c7775f6a9da9c8cb5aabd0556c228b3e70ed5
gergokutu <gergo.kutu@gmail.com>
2020-07-14 14:55:11 +0200

    Implement Add todo form UI

--- src/components/AddTodo.vue
+++ src/components/AddTodo.vue
@@ -2,7 +2,10 @@
   <div>
     <h3>Add Todo</h3>
     <div class="add">
-      
+      <form>
+        <input type="text" v-model="title" placeholder="Add Todo..."/>
+        <input type="submit" value="Submit">
+      </form>
     </div>
   </div>
 </template>
@@ -14,5 +17,23 @@ export default {
 </script>
 
 <style scoped>
+  form {
+    display: flex;
+  }
 
+  input[type="text"] {
+    flex: 10;
+    padding: 10px;
+    color: #ffffff;
+    border: 1px #41b883 solid;
+    outline: 0;
+  }
+
+  input[type="submit"] {
+    flex: 2;
+    background: #41b883;
+    color: #ffffff;
+    border: 1px #41b883 solid;
+    cursor: pointer;
+  }
 </style>
