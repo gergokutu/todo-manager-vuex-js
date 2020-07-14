6421be86447068ff422f99f645b881765cf58ae2
gergokutu <gergo.kutu@gmail.com>
2020-07-14 15:20:55 +0200

    Add new todo

--- src/components/AddTodo.vue
+++ src/components/AddTodo.vue
@@ -2,7 +2,7 @@
   <div>
     <h3>Add Todo</h3>
     <div class="add">
-      <form>
+      <form @submit="onSubmit">
         <input type="text" v-model="title" placeholder="Add Todo..."/>
         <input type="submit" value="Submit">
       </form>
@@ -11,8 +11,23 @@
 </template>
 
 <script>
+import { mapActions } from 'vuex';
+
 export default {
-  name: 'AddTodo'
+  name: 'AddTodo',
+  data() {
+    return {
+      title: ''
+    }
+  },
+  methods: {
+    ...mapActions(['addTodo']),
+    onSubmit(e) {
+      e.preventDefault();
+      this.addTodo(this.title);
+      console.log("SUBMITTED:", this.title)
+    }
+  }
 }
 </script>
 
@@ -24,7 +39,6 @@ export default {
   input[type="text"] {
     flex: 10;
     padding: 10px;
-    color: #ffffff;
     border: 1px #41b883 solid;
     outline: 0;
   }
--- src/store/modules/todos.js
+++ src/store/modules/todos.js
@@ -13,12 +13,21 @@ const actions = {
   async fetchTodos({ commit }) {
     const response = await axios.get('https://jsonplaceholder.typicode.com/todos');
 
-    commit('setTodos', response.data)
+    commit('setTodos', response.data);
+  },
+  async addTodo({ commit }, title) {
+    const response = await axios.post(
+      'https://jsonplaceholder.typicode.com/todos',
+      { title, completed: false }
+    );
+
+    commit('newTodo', response.data);
   }
 };
 
 const mutations = {
-  setTodos: (state, todos) => (state.todos = todos)
+  setTodos: (state, todos) => (state.todos = todos),
+  newTodo: (state, todo) => state.todos.unshift(todo) 
 };
 
 export default {
@@ -27,5 +36,5 @@ export default {
   getters,
   actions,
   mutations
-}
+};
 
