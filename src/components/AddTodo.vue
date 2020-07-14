88078412b492bb9acc6f0a9b6f07dbc204bb8ea0
gergokutu <gergo.kutu@gmail.com>
2020-07-14 14:47:25 +0200

    Import AddTodo into App

---
+++ src/components/AddTodo.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h3>Add Todo</h3>
+    <div class="add">
+      
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AddTodo'
+}
+</script>
+
+<style scoped>
+
+</style>
