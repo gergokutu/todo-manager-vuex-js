38ff30c952f87d0cd4fc9ace9084efd2a137160b
gergokutu <gergo.kutu@gmail.com>
2020-07-14 16:00:07 +0200

    Implement filter UI

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div id="app">
     <div class="container">
       <AddTodo />
+      <FilterTodos />
       <Todos />
     </div>
   </div>
@@ -9,13 +10,15 @@
 
 <script>
 import Todos from './components/Todos.vue';
-import AddTodo from './components/AddTodo.vue'
+import AddTodo from './components/AddTodo.vue';
+import FilterTodos from './components/FilterTodos.vue';
 
 export default {
   name: 'App',
   components: {
     Todos,
-    AddTodo
+    AddTodo,
+    FilterTodos
   }
 }
 </script>
